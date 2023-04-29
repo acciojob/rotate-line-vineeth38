a8a080686bb6dc6f8252dae027f60480e6c4af7b
vineeth <119692597+vineeth38@users.noreply.github.com>
2023-04-29 23:11:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 //your JS code here. If required.
- const lineElement = document.getElementById('line');
-    let rotation = 0;
-
-    function rotateLine() {
-      rotation += 1;
-      lineElement.style.transform = `translate(-50%, -50%) rotate(${rotation}deg)`;
-      requestAnimationFrame(rotateLine);
-    }
-
-    rotateLine();
+ var angle = 0;
+      setInterval(function() {
+        angle += 2;
+        document.getElementById("line").style.transform = "rotate(" + angle + "deg)";
+      }, 20);
